7b27edfdbf5611b87e82cdd72ac65fd4b71d5e09
Nyan Htet <nyanhtet@yahoo.com>
2022-01-09 23:02:18 -0800

    updated getKnowledgeID method due to failed test

--- event.js
+++ event.js
@@ -1,28 +1,34 @@
-const contextMenuItem = { id: "preview", title: "Knowledge Classic", contexts: ["link"], documentUrlPatterns: ["https://supportforce.lightning.force.com/*"] };
+const contextMenuItem = {
+  id: "preview",
+  title: "Knowledge Classic",
+  contexts: ["link"],
+  documentUrlPatterns: ["https://supportforce.lightning.force.com/*"],
+};
 
 //click handler
 function onClickHandler(e) {
-    let n = replaceUrl(getKnowledgeId(e));
-    chrome.windows.create({ url: n });
+  let n = replaceUrl(getKnowledgeId(e));
+  chrome.windows.create({ url: n });
 }
 
-//return a knowledge/route id from a given Salesforce Lightning URL 
+//returns a knowledge/route id from that's in the last two forward-slashes
 function getKnowledgeId(string) {
-    let firstSlash = string.lastIndexOf("/", string.lastIndexOf("/") - 1)
-    let lastSlash =  string.lastIndexOf("/") - 1);
-    return string.slice(firstSlash, lastSlash);
+  let firstSlash = string.lastIndexOf("/", string.lastIndexOf("/") - 1);
+  let lastSlash = string.lastIndexOf("/");
+  return string.slice(firstSlash + 1, lastSlash);
 }
 
-//replace the lightning URL to classic  
+//replace the lightning URL to classic
 function replaceUrl(e) {
-    return `https://supportforce.my.salesforce.com/${e}?srPos=0&srKp=kaB&lang=en_US`;
+  return `https://supportforce.my.salesforce.com/${e}?srPos=0&srKp=kaB&lang=en_US`;
 }
 
 chrome.contextMenus.removeAll(function () {
-    chrome.contextMenus.create(contextMenuItem);
+  chrome.contextMenus.create(contextMenuItem);
 }),
-    
-//event listener for Chrome, if a link specific to Salesforce is right-clicked then run the click handler
-chrome.contextMenus.onClicked.addListener(function (e) {
-    e.linkUrl.includes("https://supportforce.lightning.force.com/lightning/r/") && onClickHandler(e.linkUrl);
-});
+  //event listener for Chrome, if a link specific to Salesforce is right-clicked then run the click handler
+  chrome.contextMenus.onClicked.addListener(function (e) {
+    e.linkUrl.includes(
+      "https://supportforce.lightning.force.com/lightning/r/"
+    ) && onClickHandler(e.linkUrl);
+  });
